ad320ab8239fe5e73d3c4a0ed9ef4f162c48da04
vikkar <viktor.karaulanov@syncron.com>
2022-08-24 12:55:38 +0300

    Moved ui and syncron-sdk project configurations to angular.json. Added prettier config file. Fixed quotes

--- libs/ui/src/stories/tooltip.component.stories.ts
+++ libs/ui/src/stories/tooltip.component.stories.ts
@@ -1,11 +1,11 @@
-import { moduleMetadata, Story, Meta } from "@storybook/angular";
-import { MatSliderModule } from "@angular/material/slider";
-import { MatTooltipModule } from "@angular/material/tooltip";
+import { moduleMetadata, Story, Meta } from '@storybook/angular';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
-import { TooltipComponent } from "../lib/components/tooltip/tooltip.component";
+import { TooltipComponent } from '../lib/components/tooltip/tooltip.component';
 
 export default {
-  title: "TooltipComponent",
+  title: 'TooltipComponent',
   component: TooltipComponent,
   decorators: [
     moduleMetadata({
