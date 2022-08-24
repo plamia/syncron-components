5b6ecc98a80e4e26cabaa6ad3297a13805b902d1
Viкtor <vkaraulanov@gmail.com>
2022-08-24 14:21:07 +0300

    Moved ui and syncron-sdk project configurations to angular.json. Added prettier config file. Fixed quotes (#4)
    Co-authored-by: vikkar <viktor.karaulanov@syncron.com>

--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -1,13 +1,19 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { HttpClient, HttpClientModule } from "@angular/common/http";
-import { AgGridModule } from "ag-grid-angular";
-import { TooltipComponent } from "./components/tooltip/tooltip.component";
-import { MatSliderModule } from "@angular/material/slider";
-import { MatTooltipModule } from "@angular/material/tooltip";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { AgGridModule } from 'ag-grid-angular';
+import { TooltipComponent } from './components/tooltip/tooltip.component';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
-  imports: [CommonModule, HttpClientModule, AgGridModule, MatSliderModule, MatTooltipModule],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    AgGridModule,
+    MatSliderModule,
+    MatTooltipModule,
+  ],
   providers: [HttpClient, HttpClientModule],
   declarations: [TooltipComponent],
   exports: [AgGridModule, TooltipComponent, MatSliderModule, MatTooltipModule],
