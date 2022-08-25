5d80dcae065472dc9664cbeb9ae69f77a529f773
Viкtor <vkaraulanov@gmail.com>
2022-08-25 15:31:08 +0300

    Added scripts for e2e testing. Moved e2e projects configs to angular.json Set basing cypress configs (#5)
    Co-authored-by: vikkar <viktor.karaulanov@syncron.com>

--- apps/syncron-sdk-e2e/cypress.config.ts
+++ apps/syncron-sdk-e2e/cypress.config.ts
@@ -2,5 +2,13 @@ import { defineConfig } from 'cypress';
 import { nxE2EPreset } from '@nrwl/cypress/plugins/cypress-preset';
 
 export default defineConfig({
-  e2e: nxE2EPreset(__dirname),
+  e2e: {
+    supportFile: './src/support/e2e.ts',
+    specPattern: './src/e2e/*.{js,ts}',
+    video: true,
+    screenshotsFolder: '../../dist/cypress/apps/syncron-sdk-e2e/screenshots',
+    videosFolder: '../../dist/cypress/apps/syncron-sdk-e2e/videos',
+    trashAssetsBeforeRuns: true,
+    screenshotOnRunFailure: true,
+  }
 });
--- apps/ui-e2e/src/e2e/tooptip/tooltip.component.cy.ts
+++ apps/ui-e2e/src/e2e/tooptip/tooltip.component.cy.ts
@@ -3,4 +3,19 @@ describe('ui', () => {
   it('should render the component', () => {
     cy.get('syncron-sdk-components-tooltip').should('exist');
   });
+  it('should have a title', () => {
+    cy.get('h2').should('have.text', 'Tooltip');
+  });
+  it('should render a slider component', () => {
+    cy.get('mat-slider').should('exist').should('have.class', 'testClass');
+  });
+  it('should change color when moving slider thumb', () => {
+    cy
+      .get('.mat-slider')
+      .should('have.class', 'mat-slider-min-value')
+      .get('.mat-slider-thumb')
+      .type('{rightArrow}')
+      .get('mat-slider')
+      .should('not.have.class', 'mat-slider-min-value');
+  });
 });
